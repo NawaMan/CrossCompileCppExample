6aea2171ffcc29fd60731b5a1a21a99e2f81e38d
NawaMan <nawa@nawaman.net>
2025-07-19 01:15:14 -0400

    Further clean up.

--- docker/setup-in-docker.sh
+++ docker/setup-in-docker.sh
@@ -2,33 +2,35 @@
 
 apt-get update
 
-apt-get install -y        \
-    build-essential            \
-    ca-certificates            \
-    clang                      \
-    clang-18                   \
-    cmake                      \
-    crossbuild-essential-arm64 \
-    curl                       \
-    g++-aarch64-linux-gnu      \
-    gcc-aarch64-linux-gnu      \
-    git                        \
-    gosu                       \
-    libc++-18-dev              \
-    libc++abi-18-dev           \
-    liblzma-dev                \
-    libssl-dev                 \
-    libxml2-dev                \
-    lld                        \
-    llvm                       \
-    mingw-w64                  \
-    ninja-build                \
-    patch                      \
-    pkg-config                 \
-    python3                    \
-    python3-pip                \
-    qemu-user-static           \
-    wget                       \
+apt-get install -y               \
+    build-essential              \
+    ca-certificates              \
+    clang                        \
+    clang-18                     \
+    cmake                        \
+    crossbuild-essential-arm64   \
+    curl                         \
+    g++-aarch64-linux-gnu        \
+    gcc-aarch64-linux-gnu        \
+    git                          \
+    gosu                         \
+    libc++-18-dev                \
+    libc++abi-18-dev             \
+    libc6-dev-arm64-cross        \
+    liblzma-dev                  \
+    libssl-dev                   \
+    libstdc++-10-dev-arm64-cross \
+    libxml2-dev                  \
+    lld                          \
+    llvm                         \
+    mingw-w64                    \
+    ninja-build                  \
+    patch                        \
+    pkg-config                   \
+    python3                      \
+    python3-pip                  \
+    qemu-user-static             \
+    wget                         \
     zlib1g-dev
 
 apt-get clean
